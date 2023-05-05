ad4ef8197a20f67ef7172aceea10ecc7e60977f0
martinushka <martinushka536@yandex.ru>
2023-05-05 21:13:50 +0300

    Add logic to make flight connections

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,10 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |u| [u.location, u.id] }
-
-    @available_flights = Flight.where(search_params).order(departure_time: :asc) unless search_params.empty?
+    return if search_params.empty?
+    
+    @available_flights = Flight.where(search_params)
+    @connecting_flights = FlightConnections.new(search_params).find_connections
   end
 
   private
