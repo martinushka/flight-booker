612f088270c0e0101420c7c57c4599b90b552ea5
martinushka <martinushka536@yandex.ru>
2023-05-07 22:12:43 +0300

    Update tests and factories

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -16,8 +16,8 @@ require 'rails_helper'
 RSpec.describe Passenger, type: :model do
     subject(:passenger) { described_class.new }
   
-    describe 'associations' do
-      it { is_expected.to belong_to(:booking) }
-      it { is_expected.to have_many(:flights).through(:booking) }
+    describe 'validations' do
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_presence_of(:email) }
     end
   end
--- spec/requests/bookings_request_spec.rb
+++ spec/requests/bookings_request_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-# RSpec.describe "Bookings", type: :request do
-
-# end
+RSpec.describe "Bookings", type: :request do
+    it "handles a missing booking correctly" do
+      get booking_path("not-here")
+      expect(response).to redirect_to(root_url)
+      expect(flash[:alert]).to eq("Sorry, this booking does not exist.")
+    end
+  end
